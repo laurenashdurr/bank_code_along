397aee783b624324bfd89c062078b0fd3c2fb714
Lauren Durr <laurenashdurr@gmail.com>
2021-04-24 16:19:32 -0400

    (end of demo - Lauren) added a reset button and amount field data validation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let form = document.getElementById("transaction-form")
 let balanceAmount = document.getElementById("account-balance")
 let transactionLog = document.getElementById("transaction-log")
+let resetButton = document.getElementById("reset-button")
 
 let balance = 0.00
 let transactionNumber = 0
@@ -8,21 +9,30 @@ let transactionArr = []
 
 form.onsubmit = (e) => {
 
-    let type;
+    let amount = Number(e.target.amount.value)
+    if (amount > 0 || amount < 0) {
 
-    if (e.target["transaction-type"].value === "1") type = "Deposit";
-    else type = "Withdraw";
+        let transaction = {
+            type: e.target["transaction-type"].value,
+            amount: Number(e.target.amount.value)
+        }
+        // console.log(transaction)
+        handleTransaction(transaction);
+        renderBalance();
+        renderTransactionTable();
 
-    let transaction = {
-        type: type ,  
-        amount: Number(e.target.amount.value)
     }
-    // console.log(transaction)
-    handleTransaction(transaction);
-    renderBalance();
-    renderTransactionTable();
+
 
     e.preventDefault();
+}
+
+resetButton.onclick = () => {
+    balance = 0; 
+    transactionNumber = 0;
+    transactionArr = [];
+    renderBalance();
+    renderTransactionTable();
 }
 
 function handleTransaction(transaction) {
@@ -35,7 +45,7 @@ function handleTransaction(transaction) {
         preBalance: balance
 
     }
-    
+
     if (transaction.type === "Deposit") {
         balance += transaction.amount
     } else {
@@ -45,21 +55,20 @@ function handleTransaction(transaction) {
 
     currentTransaction.postBalance = balance
     transactionArr.push(currentTransaction)
-    console.log(balance)
-    console.log(transactionArr)
     
+
 }
 
-function renderBalance () {
+function renderBalance() {
     balanceAmount.innerText = `$ ${balance}`
     balanceAmount.style.color = balance >= 0 ? `#9AB278` : `#FE4C58`
 
 }
 
-function renderTransactionTable () {
+function renderTransactionTable() {
     transactionLog.innerHTML = ""
 
-    for (let i = transactionArr.length -1; i >= 0; i --) {
+    for (let i = transactionArr.length - 1; i >= 0; i--) {
         let row = createTableRow(transactionArr[i])
         transactionLog.appendChild(row)
     }
@@ -68,24 +77,24 @@ function renderTransactionTable () {
 
 function createTableRow(entry) {
     let tableRow = document.createElement("tr")
-        let tID = document.createElement("th")
-        let tType = document.createElement("td")
-        let tAmount = document.createElement("td")
-        let preBalance = document.createElement("td")
-        let postBalance = document.createElement("td")
+    let tID = document.createElement("th")
+    let tType = document.createElement("td")
+    let tAmount = document.createElement("td")
+    let preBalance = document.createElement("td")
+    let postBalance = document.createElement("td")
 
-        tID.innerText = entry.tID
-        tType.innerText = entry.type
-        tAmount.innerText = entry.amount
-        preBalance.innerText = entry.preBalance
-        postBalance.innerText = entry.postBalance
+    tID.innerText = entry.tID
+    tType.innerText = entry.type
+    tAmount.innerText = entry.amount
+    preBalance.innerText = entry.preBalance
+    postBalance.innerText = entry.postBalance
 
-        tableRow.appendChild(tID)
-        tableRow.appendChild(tType)
-        tableRow.appendChild(tAmount)
-        tableRow.appendChild(preBalance)
-        tableRow.appendChild(postBalance)
+    tableRow.appendChild(tID)
+    tableRow.appendChild(tType)
+    tableRow.appendChild(tAmount)
+    tableRow.appendChild(preBalance)
+    tableRow.appendChild(postBalance)
 
-       return tableRow
+    return tableRow
 
 }
